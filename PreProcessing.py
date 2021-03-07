79517c27eda57bb735776f79c2e32d1d2bc7a2a4
EvgeniiaJ <78631584+EvgeniiaJ@users.noreply.github.com>
2021-03-06 23:03:41 -0500

    Update PreProcessing.py

--- PreProcessing.py
+++ PreProcessing.py
@@ -1,12 +1,12 @@
 class PreProcessing:
     
-    def __init__():
-        return
+    def __init__(self):
+        pass
     
     # make all letters of the entire document lowercase
     # pad each distinct sentence in the data set with start (<s>) 
     #       and end (</s>) symbols
-    def pad_file_data(input_file):
+    def pad_file_data(self, input_file):
         
         padded_data = []        
         file = open(input_file, 'r', encoding = 'utf-8')
@@ -26,7 +26,7 @@ class PreProcessing:
     
     # make a dictionary of words from the data set
     
-    def create_dictionary(padded_data):
+    def create_dictionary(self, padded_data):
         word_dict = dict()
         
         for word in padded_data:
@@ -39,13 +39,13 @@ class PreProcessing:
     
     
     # mark words with unknown symbol (<unk>) if they are seen no more than once
-    def mark_unknown_words(pass_id, data, file_dict):
+    def mark_unknown_words(self, pass_id, data, file_dict):
         
         marked_data = []
         
         if pass_id == 1:
             for word in data:
-                if file_dict[data] == 1:
+                if file_dict[word] == 1:
                     marked_data.append('<unk>')
                 else:
                     marked_data.append(word)
